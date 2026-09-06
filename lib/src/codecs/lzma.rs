1313284ee1fd344454975e26dbda8ad20aa9a747
Ludea <kudealol.olol@gmail.com>
2026-09-06 20:00:24 +0200

    fix: make clippy happy

--- lib/src/codecs/lzma.rs
+++ lib/src/codecs/lzma.rs
@@ -4,8 +4,8 @@ use super::Coder;
 use crate::io;
 
 enum Inner<W: io::Write> {
-    Encoder(LzmaWriter<W>),
-    Decoder { w: W, stream: LzmaStream },
+    Encoder(Box<LzmaWriter<W>>),
+    Decoder { w: W, stream: Box<LzmaStream> },
 }
 
 pub struct Writer<W: io::Write> {
@@ -15,14 +15,13 @@ pub struct Writer<W: io::Write> {
 impl<W: io::Write> Writer<W> {
     pub fn decompressor(w: W) -> Result<Self, io::Error> {
         let stream = LzmaStream::new_mem_limit(u32::MAX, None);
-        Ok(Self { inner: Inner::Decoder { w, stream } })
+        Ok(Self { inner: Inner::Decoder { w, stream: Box::new(stream) } })
     }
 
     pub fn compressor(w: W, preset: u32) -> Result<Self, io::Error> {
         let options = LzmaOptions::with_preset(preset);
-        let enc = LzmaWriter::new_use_header(w, &options, None)
-            .map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
-        Ok(Self { inner: Inner::Encoder(enc) })
+        let enc = LzmaWriter::new_use_header(w, &options, None).map_err(io::Error::other)?;
+        Ok(Self { inner: Inner::Encoder(Box::new(enc)) })
     }
 }
 
@@ -36,13 +35,13 @@ impl<W: io::Write> Coder<W> for Writer<W> {
 
     fn finish(self) -> io::Result<W> {
         match self.inner {
-            Inner::Encoder(w) => w.finish().map_err(|e| io::Error::new(io::ErrorKind::Other, e)),
+            Inner::Encoder(w) => w.finish().map_err(io::Error::other),
             Inner::Decoder { mut w, mut stream } => {
                 let mut buffer = [0u8; io::BUFFER_SIZE];
                 loop {
                     let res = stream
                         .process(&[], &mut buffer, Action::Finish)
-                        .map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
+                        .map_err(io::Error::other)?;
                     w.write_all(&buffer[..res.bytes_produced])?;
                     if res.status == Status::StreamEnd {
                         return Ok(w);
@@ -67,7 +66,7 @@ impl<W: io::Write> io::Write for Writer<W> {
                 while !input.is_empty() {
                     let res = stream
                         .process(input, &mut buffer, Action::Run)
-                        .map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
+                        .map_err(io::Error::other)?;
                     input = &input[res.bytes_consumed..];
                     w.write_all(&buffer[..res.bytes_produced])?;
                     if res.status == Status::StreamEnd {
